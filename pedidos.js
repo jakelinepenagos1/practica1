d08c5123fa59f17dbab1f7fe6c45ad96047c4a67
jakelinepenagos1 <70981425+jakelinepenagos1@users.noreply.github.com>
2021-09-01 17:03:07 -0500

    1

--- pedidos.js
+++ pedidos.js
@@ -1,3 +1,4 @@
+
 let pedidos=[
 
     {
@@ -13,9 +14,108 @@ let pedidos=[
     {
         id:2,
         nombre:"xbox",
+        precio:700,
+        peso:13.1,
+        cantidad:1,
+        foto:"img/xbox.jpg"
+    },
+    {
+        id:4,
+        nombre:"Nintendo Switch",
+        precio:409,
+        peso:4.75,
+        cantidad:3,
+        foto:"img/nintendo.jpg"
+    },
+    {
+        id:5,
+        nombre:"AirPods generacion 1",
+        precio:35,
+        peso:38,
+        cantidad:2,
+        foto:"img/airpods.jpg"
+    },
+
+
+    {
+        id:7,
+        nombre:"HUAWEI P40",
+        precio:780,
+        peso:4.48,
+        cantidad:6,
+        foto:"img/huawei p40.jpg"
+    },
+
+    
+    {
+        id:8,
+        nombre:"MAC (computador de mesa)",
+        precio:750,
+        peso:14.5,
+        cantidad:1,
+        foto:"img/mac.jpg"
+    },
+    {
+        id:9,
+        nombre:"MAC (portatil)",
+        precio:750,
+        peso:14.5,
+        cantidad:8,
+        foto:"img/mac2.jpg"
+    },
+    
+    {
+        id:6,
+        nombre:"AirPods generacion 2",
+        precio:750,
+        peso:14.5,
+        cantidad:3,
+        foto:"img/airpods2.jpg"
+    },
+
+    {
+        id:11,
+        nombre:"pencil apple generacion 2",
+        precio:750,
+        peso:14.5,
+        cantidad:10,
+        foto:"img/pencil apple g2.jpg"
+    },
+
+    
+    {
+        id:12,
+        nombre:"Tablet HUAWEI",
+        precio:750,
+        peso:14.5,
+        cantidad:5,
+        foto:"img/tablet huaweit10.jpg"
+    },
+    {
+        id:13,
+        nombre:"Tablet SAMSUNG",
+        precio:750,
+        peso:14.5,
+        cantidad:6,
+        foto:"img/ps5.jpg"
+    },
+
+    {
+        id:15,
+        nombre:"XIAOME",
         precio:750,
         peso:14.5,
         cantidad:2,
+        foto:"img/xiaomi.jpg"
+    },
+
+    
+    {
+        id:16,
+        nombre:"xbox",
+        precio:700,
+        peso:13.1,
+        cantidad:5,
         foto:"img/xbox.jpg"
     }
 ];
@@ -26,6 +126,7 @@ pedidos.forEach(function(pedido){
 
  let columna=document.createElement("div");
  columna.classList.add("col");
+ columna.classList.add("p-4");
 
  let cuerpo=document.createElement("div");
  cuerpo.classList.add("card");
@@ -53,6 +154,22 @@ pedidos.forEach(function(pedido){
  parrafo3.classList.add("card-text");
  parrafo3.textContent=`peso: ${pedido.peso}`;  
 
+ let boton1=document.createElement("button");
+ boton1.classList.add("btn-outline-dark");
+ boton1.classList.add("col-md-5");
+
+ let boton2=document.createElement("button");
+ boton2.classList.add("btn-outline-dark");
+ boton2.classList.add("col-md-5");
+ boton2.classList.add("ms-2");
+
+ let icono1=document.createElement("i");
+ icono1.classList.add("fa-trash-alt");
+ icono1.classList.add("fas");
+
+ let icono2=document.createElement("i");
+ icono2.classList.add("fa-pencil-alt");
+ icono2.classList.add("fas");
 
 contenedorPadre.appendChild(columna);
 columna.appendChild(cuerpo);
@@ -62,4 +179,10 @@ body.appendChild(titulo);
 body.appendChild(parrafo1);
 body.appendChild(parrafo2);
 body.appendChild(parrafo3);
+body.appendChild(boton1);
+body.appendChild(boton2);
+boton1.appendChild(icono1);
+boton2.appendChild(icono2);
+
+
 } )
